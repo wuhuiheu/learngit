098c235194c993f0756b25cee001ea729b5a7735
wuhuiheu <wuhuiheu@gmail.com>
2016-01-17 17:20:46 +0800

    添加一个1加到100的方法

--- src/com/example/learngit/MainActivity.java
+++ src/com/example/learngit/MainActivity.java
@@ -2,14 +2,25 @@ package com.example.learngit;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends Activity {
-
+	
+	private int add1To100(){
+		int result = 0;
+		for(int i = 1; i < 100; i++){
+			result += i;
+		}
+		return result;
+	}
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("ljh", "1+..+100:" + add1To100());
+        
     }
 }
